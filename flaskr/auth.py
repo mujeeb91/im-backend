97a4ecf7b22b1e847c22a37635632ee014ee1b8e
Mujeeb <mujeeb91@hotmail.com>
2019-12-21 10:10:56 +0500

    Used JWT token for authorization.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,17 +1,42 @@
-from flask import Blueprint, request, Response
+from datetime import datetime, timedelta
+from flask import Blueprint, current_app as app, jsonify, request, Response
 from flask_api import status
+import jwt
+from jwt.exceptions import ExpiredSignatureError
+from functools import wraps
+
 from .model import User, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization', '').replace('Token ', '')
+        if not token:
+            return jsonify({'message': 'Token is missing'}), status.HTTP_403_FORBIDDEN
+        try:
+            jwt.decode(token, app.config['SECRET_KEY'])
+        except ExpiredSignatureError:
+            return jsonify({'message': 'Token is invalid'}), status.HTTP_403_FORBIDDEN
+        return f(*args, **kwargs)
+    return decorated
+
+
 @auth.route('/login', methods=['POST'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
-
+    username = request.json.get('username')
+    password = request.json.get('password')
     user = User.query.filter_by(username=username).first()
+    if user and check_password_hash(user.password, password):
+        token = jwt.encode(
+            {
+                'username': username,
+                'exp': datetime.utcnow() + timedelta(minutes=10)
+            },
+            app.config['SECRET_KEY']
+        )
+        return jsonify({'token': token.decode('UTF-8')}), status.HTTP_200_OK
+    return jsonify({'message': 'Invalid credentials'}), status.HTTP_401_UNAUTHORIZED
 
-    if not user or not check_password_hash(user.password, password):
-        return Response(status=status.HTTP_401_UNAUTHORIZED)
-    return Response(status=status.HTTP_200_OK)
